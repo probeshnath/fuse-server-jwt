dda84e15157e1f7a174cf695e0d5034b93a745ea
probeshnath <probeshn@gmail.com>
2023-11-04 20:51:52 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const express = require("express");
 const app = express();
+const jwt = require('jsonwebtoken');
 const cors = require("cors")
+const cookieParser = require("cookie-parser");
+require("dotenv").config()
 const port = process.env.Port || 5000;
 
 
 // middleware
-app.use(cors());
+// app.use(cors(
+//   {
+//   origin: [
+//     "http://localhost:5173/",
+//     "http://localhost:5174/"
+//   ],
+//   credentials:true
+// }
+// ));
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials:true
+}))
 app.use(express.json())
+app.use(cookieParser());
 
 
 // mongoBD
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://probeshn:ULftU6LeGFL6QBWq@cluster0.y8cef5u.mongodb.net/?retryWrites=true&w=majority";
+// const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.y8cef5u.mongodb.net/?retryWrites=true&w=majority`;
+var uri = `mongodb://probeshn:${process.env.MONGODB_PASSWORD}@ac-iy6n83p-shard-00-00.y8cef5u.mongodb.net:27017,ac-iy6n83p-shard-00-01.y8cef5u.mongodb.net:27017,ac-iy6n83p-shard-00-02.y8cef5u.mongodb.net:27017/?ssl=true&replicaSet=atlas-y02b2q-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,6 +39,35 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+const logger = async ( req,res, next) =>{
+  console.log("logger called", req.host, req.originalUrl)
+  next();
+}
+
+const verifyToken = async (req,res,next) =>{
+  const token = req.cookies?.token;
+  console.log("token from varify ", token)
+
+  if(!token){
+    return res.status(401).send({message:"not authriszed"})
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET,(err,decoded)=>{
+    if(err){
+     
+      return res.status(401).send({message:"not authriszed"})
+    }
+
+    console.log("vale of the decoded", decoded)
+
+    req.user = decoded
+
+    next();
+  })
+
+}
+
 
 async function run() {
   try {
@@ -51,10 +97,32 @@ async function run() {
     })
 
     // get all products
-    app.get("/products",async(req,res)=>{
-      const cursor = await productsDB.find();
+    app.get("/products", logger,verifyToken ,async(req,res)=>{
+      // console.log("tok tok token", req.cookies.token)
+      console.log("i love you user ", req.user)
+
+      if(req.query?.email !==  req.user.email){
+        return res.status(403).send({message: "forbidden access"})
+      }
+      const cursor =  productsDB.find();
       const result = await cursor.toArray();
       res.send(result)
+    })
+
+    // jwt
+
+    app.post("/jwt",logger, async(req,res)=>{ 
+      const user = req.body;
+      console.log("from jwt",user)
+      const token = jwt.sign(user, process.env.JWT_SECRET,{expiresIn:"1h"})
+
+      res
+      .cookie("token", token,{
+        httpOnly:true,
+        secure: true,
+        sameSite:"none"
+      })
+      .send({success:true})
     })
 
 
